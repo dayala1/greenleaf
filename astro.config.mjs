3096a85e218819cb3a89724896ea52a0ad7fe614
Daniel Ayala <danielayalahernandez@hotmail.com>
2025-06-16 17:38:18 +0200

    removed base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dayala1.github.io',
-  base: '/greenleaf',
   vite: {
     assetsInclude: ['**/*.glb', '**/*.gltf'],
     plugins: [tailwindcss()],
